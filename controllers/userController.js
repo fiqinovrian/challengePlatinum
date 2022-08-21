01fc1c0223c4dae154576afccd2080b584800d24
fiqinovrian <fiqinovrian@gmail.com>
2022-08-21 20:45:25 +0700

    Fix user update by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require('../models').User;
+const { Op } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 const create = async function (req, res) {
     const cekEmail = await User.findOne({ where: { email: req.body.email } });
@@ -50,34 +52,90 @@ const getById = async (req, res) => {
     }
 }
 
-const updateById = (req, res) => {
-    const cekEmail =  User.findOne({ where: { id: req.params.id, email: req.body.email } });
-    if (cekEmail === null) {
-        const pass = req.body.password;
-        if (pass.length < 8) {
-            return res.render('register', { message: 'Password At Least 8 Characters' });
-        } else {
-            const encryptedPassword = bcrypt.hashSync(pass, 10);
-            const data = {
-                name: req.body.name,
-                email: req.body.email,
-                gender: req.body.gender,
-                role: req.body.role,
-                password: encryptedPassword
+const updateById = async (req, res) => {
+    //ambil user dengan id yang diminta, kemudian cek nih, kalo dy update dengan email yang sama, maka jalankan updatenya
+    //kalo email yang dia inputkan itu beda, cek dlu email di database udah ada belum, kalo belum jalankan updatenya
+
+    const userId = req.params.id;
+    try {
+        const isUserExist =  await User.findByPk(userId);
+        if(isUserExist != null) {
+            const inputEmail = req.body.email;
+            const emailUser = isUserExist.email;
+            if(inputEmail === emailUser) {
+                const pass = req.body.password;
+                if(pass.length < 8) {
+                    return res.status(500).json('Password At Least 8 Character');
+                }
+                const encryptedPassword = bcrypt.hashSync(pass, 10);
+                const updateUser = User.update({
+                    name: req.body.name,                    
+                    email: req.body.email,
+                    gender: req.body.gender,
+                    role: req.body.role,
+                    password: encryptedPassword
+                }, {
+                    where: {
+                        id: req.params.id
+                    }
+                });
+                return res.status(200).json('User berhasil di update');
+            } else {
+                const emailCheck =  User.findOne({ where: { id: { [Op.not]: userId }, email: req.body.email } });
+                if (emailCheck != null) {
+                    return res.status(500).json('Email has been already taken');
+                }
+                const pass = req.body.password;
+                if (pass.length < 8) {
+                    return res.status(500).json('Password At Least 8 Character');
+                }
+                const encryptedPassword = bcrypt.hashSync(pass, 10);
+                const updateUser = User.update({
+                    name: req.body.name,
+                    email: req.body.email,
+                    gender: req.body.gender,
+                    role: req.body.role,
+                    password: encryptedPassword
+                }, {
+                    where: {
+                        id: req.params.id
+                    }
+                });
+                return res.status(200).json(updateUser);
             }
-            User.update(data)
-                .then(data => {
-                    res.status(201).json(data)
-                })
-                .catch(err => {
-                    // return res.render('register', { message: 'Internal Server Error' });
-                    return res.status(500).json('Internal server error');
-                })
         }
-    } else {
-        return res.render('register', { message: 'Email has been already taken' });
-        // return res.json('Email has been already taken');
+        return res.status(500).json('User dengan Id tersebut tidak ditemukan')
     }
+    catch(err) {
+        return res.status(500).json(err);
+    }
+    // const cekEmail =  User.findOne({ where: { id: req.params.id, email: req.body.email } });
+    // if (cekEmail === null) {
+    //     const pass = req.body.password;
+    //     if (pass.length < 8) {
+    //         return res.render('register', { message: 'Password At Least 8 Characters' });
+    //     } else {
+    //         const encryptedPassword = bcrypt.hashSync(pass, 10);
+    //         const data = {
+    //             name: req.body.name,
+    //             email: req.body.email,
+    //             gender: req.body.gender,
+    //             role: req.body.role,
+    //             password: encryptedPassword
+    //         }
+    //         User.update(data)
+    //             .then(data => {
+    //                 res.status(201).json(data)
+    //             })
+    //             .catch(err => {
+    //                 // return res.render('register', { message: 'Internal Server Error' });
+    //                 return res.status(500).json('Internal server error');
+    //             })
+    //     }
+    // } else {
+    //     return res.render('register', { message: 'Email has been already taken' });
+    //     // return res.json('Email has been already taken');
+    // }
 }
 
 const deleteById = (req, res) => {
